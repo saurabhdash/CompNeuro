4150d5fd386e9e6a762778550ffd75e5e4474c6c
saurabhdash <saurabhdash2512@gmail.com>
2017-08-29 04:31:06 +0530

    Done till Question 8

--- Project2_MLE.m
+++ Project2_MLE.m
@@ -145,6 +145,12 @@ hold on;
 plot(V,V_nc); % V Null cline
 xlim([-84 120])
 ylim([0 1])
+figure(6);
+plot(V,w_nc); % w Null cline
+hold on;
+plot(V,V_nc); % V Null cline
+xlim([-84 120])
+ylim([0 1])
 %%
 syms V;
 J = jacobian([(1/C)*(Iext-(gca*m_inf*(V-Vca)+ gk*w*(V-Vk) + gl*(V-Vl))), phi*(w_inf-w)/Tw], [V, w]);
@@ -152,15 +158,13 @@ V = S.V;
 w = S.w;
 Jeq = double(subs(J));
 Eign_eq = eig(Jeq)
-% syms V w;
-% jac=jacobian([(1/C)*(Iext-gca*0.5*(1+tanh((V-V1)/V2)).*(V-Vca)-gk*w.*(V-Vk)-gl*(V-Vl)), phi*(0.5*(1+tanh((V-V3)/V4))-w).*cosh((V-V3)/(2*V4))], [V,w]);
-% jac=double(subs(jac,[V,w],[Veq1,weq1]));
-% eigenvalues=eig(jac)
 %%
 [t1,S1]=ode15s(@MLE, [0 300], [ -60.855, 0.014]);
 [t2,S2]=ode15s(@MLE, [0 300], [ -27.952, 0.119]);
 [t3,S3]=ode15s(@MLE, [0 300], [-27.9, 0.17]);
-%plot(S1(:,1), S1(:,2),'r',S2(:,1), S2(:,2),'b',S3(:,1), S3(:,2),'g');
+figure(5);
+hold on;
+plot(S1(:,1), S1(:,2),'r',S2(:,1), S2(:,2),'b',S3(:,1), S3(:,2),'g');
 figure;
 plot(t1,S1(:,1),'r');
 hold on;
@@ -169,12 +173,81 @@ plot(t3,S3(:,1),'g');
 figure(5);
 hold on;
 [t3,S3]=ode15s(@MLE, [2000 0], [-27.952, 0.119]);
-plot(S3(:,1), S3(:,2));
+K_ms = convhull(S3(:,1),S3(:,2));
+plot(S3(K_ms,1), S3(K_ms,2));
+figure(6);
 hold on;
-[t3,S3]=ode15s(@MLE, [0 1000], [-20, 0.1376]);
 plot(S3(:,1), S3(:,2));
-[t3,S3]=ode15s(@MLE, [0 1000], [-19.8, 0.1376]);
-plot(S3(:,1), S3(:,2));
+%hold on;
+%[t3,S3]=ode15s(@MLE, [0 1000], [-20, 0.1376]);
+%plot(S3(:,1), S3(:,2));
+%[t3,S3]=ode15s(@MLE, [0 1000], [-19.8, 0.1376]);
+%plot(S3(:,1), S3(:,2));
+
+max_v = 0;
+%figure;
+for v = -22:0.1:-17
+    [t1,S1]=ode15s(@MLE, [0 1000], [v  , 0.1376]);
+    %plot(S1(:,1), S1(:,2));
+    %xlim([-84 120])
+    %ylim([0 1])
+    %hold on;
+    max_v = [max_v max(S1(:,1))];
+end
+figure;
+plot(-22:0.1:-17,max_v(2:end));
+%% Part 9
+syms V w m_inf w_inf Tw;
+m_inf = 0.5*(1+tanh((V-V1)/V2));
+w_inf = 0.5*(1+tanh((V-V3)/V4));
+Tw = 1/cosh((V-V3)/(2*V4));
+Iext = 80;
+eqns = [ phi*(w_inf-w)/Tw == 0, Iext - gca*m_inf*(V-Vca) - gk*w*(V-Vk) - gl*(V-Vl) == 0];
+vars = [V w];
+S = vpasolve(eqns, vars);
+[S.V S.w]
+w1 = (Iext-gca*m_inf*(V-Vca)-gl*(V-Vl))/(gk*(V-Vk));
+V = -83:0.1:120;
+w_nc = double(subs(w_inf));
+V_nc = double(subs(w1));
+figure;
+plot(V,w_nc); % w Null cline
+hold on;
+plot(V,V_nc); % V Null cline
+xlim([-84 120])
+ylim([0 1])
+syms V w
+Iext = 86;
+eqns = [ phi*(w_inf-w)/Tw == 0, Iext - gca*m_inf*(V-Vca) - gk*w*(V-Vk) - gl*(V-Vl) == 0];
+vars = [V w];
+S = vpasolve(eqns, vars);
+[S.V S.w]
+w1 = (Iext-gca*m_inf*(V-Vca)-gl*(V-Vl))/(gk*(V-Vk));
+V = -83:0.1:120;
+w_nc = double(subs(w_inf));
+V_nc = double(subs(w1));
+figure;
+plot(V,w_nc); % w Null cline
+hold on;
+plot(V,V_nc); % V Null cline
+xlim([-84 120])
+ylim([0 1])
+syms V w
+Iext = 90;
+eqns = [ phi*(w_inf-w)/Tw == 0, Iext - gca*m_inf*(V-Vca) - gk*w*(V-Vk) - gl*(V-Vl) == 0];
+vars = [V w];
+S = vpasolve(eqns, vars);
+[S.V S.w]
+w1 = (Iext-gca*m_inf*(V-Vca)-gl*(V-Vl))/(gk*(V-Vk));
+V = -83:0.1:120;
+w_nc = double(subs(w_inf));
+V_nc = double(subs(w1));
+figure;
+plot(V,w_nc); % w Null cline
+hold on;
+plot(V,V_nc); % V Null cline
+xlim([-84 120])
+ylim([0 1])
  end
 
 function dS = MLE(t,S)
